f50fbf1ad9282674d1cb44ba47cbd51c95652404
Ishwarya8463 <144302380+Ishwarya8463@users.noreply.github.com>
2023-09-25 19:13:51 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,5 +1,16 @@
-year=int(input("Enter the year to be checked:"))
-if (year % 4 == 0 and year % 100 != 9 or year % 400 == 0):
-  print("The year is a leap year!")
+
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
+
+# Example usage:
+products = ["apple", "banana", "apple", "orange", "apple"]
+target = "apple"
+result = linear_search_product(products, target)
+if result:
+    print(f"The product '{target}' was found at indices: {result}")
 else:
-  print("The  year isn't a leap year!")
+    print(f"The product '{target}' was not found in the list.")
